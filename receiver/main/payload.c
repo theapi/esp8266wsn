c3a3a301c74ba2482fb75b98d29eaae39470ca68
theapi <petermclarke@gmail.com>
2019-12-14 07:56:31 +0000

    Mac address first in the payload

--- receiver/main/payload.c
+++ receiver/main/payload.c
@@ -5,13 +5,14 @@
 void PAYLOAD_serialize(PAYLOAD_sensor_t *payload, uint8_t buffer[sizeof(PAYLOAD_sensor_t)]) {
   memset(buffer, 0, sizeof(PAYLOAD_sensor_t));
   int b = 0;
-  buffer[b++] = payload->message_type;
-  buffer[b++] = payload->message_id;
 
   /* Do the mac address */
   for (int i = 0; i < 6; i++) {
     buffer[b++] = payload->mac[i];
   }
+
+  buffer[b++] = payload->message_type;
+  buffer[b++] = payload->message_id;
 
   /* Do the adc values. */
   for (int i = 0; i < PAYLOAD_ADC_NUM; i++) {
@@ -30,13 +31,14 @@ void PAYLOAD_serialize(PAYLOAD_sensor_t *payload, uint8_t buffer[sizeof(PAYLOAD_
 
 void PAYLOAD_unserialize(PAYLOAD_sensor_t *payload, uint8_t buffer[sizeof(PAYLOAD_sensor_t)]) {
   int b = 0;
-  payload->message_type = buffer[b++];
-  payload->message_id = buffer[b++];
 
   /* Do the mac address */
   for (int i = 0; i < 6; i++) {
     payload->mac[i] = buffer[b++];
   }
+
+  payload->message_type = buffer[b++];
+  payload->message_id = buffer[b++];
 
   /* Do the adc values. */
   for (int i = 0; i < PAYLOAD_ADC_NUM; i++) {
