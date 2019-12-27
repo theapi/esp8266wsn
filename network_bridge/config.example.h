83a7bfefa763fe5638f20f2ea065209de04cf05b
Peter <petermclarke@gmail.com>
2019-12-27 12:41:48 +0000

    Update config.example.h

--- network_bridge/config.example.h
+++ network_bridge/config.example.h
@@ -1,11 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* ssid = "VM9666838";
-const char* password = "jxfvzpXzb9mg";
+const char* ssid = "xxxxx";
+const char* password = "xxxx";
 
-// Set the MQTT server details
-#define MQTT_SERVER "192.168.0.3"
-#define MQTT_PORT 1883
 
 #endif
